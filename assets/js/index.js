13d98ccfcdc15a45ee71631c54e73d4b5efc2311
KeshavMajithia <65853673+KeshavMajithia@users.noreply.github.com>
2020-12-06 18:39:26 +0530

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+var firebaseConfig = 
+{
+    apiKey: "AIzaSyCxkVXvWgLDwj0KKE75G1ij6FbounLyzeQ",
+    authDomain: "eduquix.firebaseapp.com",
+    databaseURL: "https://eduquix.firebaseio.com",
+    projectId: "eduquix",
+    storageBucket: "eduquix.appspot.com",
+    messagingSenderId: "832543363823",
+    appId: "1:832543363823:web:bbc9fd8f4936496d91c11a",
+    measurementId: "G-7YSCXJRJ13"
+  };
+// Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
